355e1813195f7208bab5f7d756d29d21f6f5caaf
tammykhan <tammykhan543@gmail.com>
2026-03-15 22:46:37 +0600

    Make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,11 +4,7 @@ import { formatCurrency } from "../utils/money.js";
 import {hello} from "https://unpkg.com/supersimpledev@1.0.1/hello.esm.js"
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOptions.js";
-
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, 'days');
-console.log(deliveryDate.format('dddd, MMMM D'));
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
 
@@ -118,6 +114,7 @@ export function renderOrderSummary() {
     }
 
     document.querySelector('.js-order-summary').innerHTML = cartSummaryHtml;
+
     document.querySelectorAll('.js-delete-link').forEach((link) => {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
@@ -125,6 +122,8 @@ export function renderOrderSummary() {
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
+
+        renderPaymentSummary();
       })
     });
 
@@ -134,6 +133,7 @@ export function renderOrderSummary() {
 
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       })
     })
 }

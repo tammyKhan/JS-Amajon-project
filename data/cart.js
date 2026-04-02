4f12075dc3fd343eb856d250a76c0d460960357c
tammykhan <tammykhan543@gmail.com>
2026-04-02 22:30:58 +0600

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,9 @@
-export let cart = JSON.parse(localStorage.getItem('cart'));
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+     cart = JSON.parse(localStorage.getItem('cart'));
 
 if(!cart) {
     cart =  [
@@ -13,6 +18,7 @@ if(!cart) {
     deliveryOptionId: '2'
   },
 ];
+}
 }
 
 function saveToStorage() {
